a63aa038dfd593ec739ebac108e835d9fd3d9bf7
Graeme Boy <graemeboy@gmail.com>
2014-06-21 10:18:16 +0200

    Update views directory

---
+++ app/views/admin/includes/footer.php
@@ -0,0 +1,9 @@
+</div>
+<!-- .container -->
+</div>
+<!-- #admin-content .col-sm-9 -->
+</div>
+<!-- .container-fluid -->
+</body>
+
+</html>
---
+++ app/views/admin/includes/header.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <div id="admin-wrap" class="container-fluid">
+        <div id="admin-menu" class="col-sm-3">
+            <?php include_once('navigation/nav.php') ?>
+        </div>
+        <div id="admin-content" class="col-sm-9">
+        
