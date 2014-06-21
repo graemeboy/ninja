dc4604cc54a4c1036418f9a2cab27109268f4b9c
Graeme Boy <graemeboy@gmail.com>
2014-06-21 10:52:12 +0200

    Add admin header

--- app/views/admin/includes/header.php
+++ app/views/admin/includes/header.php
@@ -1,11 +1,32 @@
 <html>
 
 <head>
-    <title></title>
+    <title><?php echo $meta_title ?></title>
+    <link href="/public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <style type="text/css">
+        #admin-header-wrap {
+            background-color: #333;
+            padding: 8px;
+        }
+        #admin-header-wrap, #admin-header-wrap a {
+            color:#fff;   
+        }
+    </style>
 </head>
 
 <body>
-    <div id="admin-wrap" class="container-fluid">
+    <div id="admin-wrap">
+        <div id="admin-header-wrap">
+            <div id="admin-header-left" class="container-fluid pull-left">
+              <?php echo $head_title ?>  
+            </div>
+            <div id="admin-header-right" class="container-fluid pull-right">
+                <a href="" class="admin-logout-link">Logout</a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        
         <div id="admin-menu" class="col-sm-3">
             <?php include_once('navigation/nav.php') ?>
         </div>
