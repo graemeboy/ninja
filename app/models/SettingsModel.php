21ef1d640b0fb9796984520c9b41832366a69b16
Graeme Boy <graemeboy@gmail.com>
2014-06-28 23:32:49 +0200

    Add settings model

---
+++ app/models/SettingsModel.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * SettingsModel
+ * Provides functions to get and set user settings.
+ */
+class SettingsModel {
+
+	// Define default settings, in case the user wants to restore.
+	public static $defaultSettings = array (
+		'theme' => 'default',
+		'style' => 'cerulean',
+	);
+
+	function __construct () {
+
+
+	}
+
+	function setTheme() {
+
+
+	}
+
+	function setStyle() {
+
+
+	}
+
+	function restoreDefaults() {
+
+
+	}
+
+
+
+}
+
+?>
