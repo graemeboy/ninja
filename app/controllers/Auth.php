293dd480bffcc436e3b0a95b8d2df82cb517978e
Graeme Boy <graemeboy@gmail.com>
2014-06-28 23:54:25 +0200

    Update controllers, add Auth controller

---
+++ app/controllers/Auth.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Class Auth
+ * Contains controller functions for signing in and out of the admin area.
+ */
+
+class Auth {
+
+	function __construct () {
+
+
+	}
+	
+	function signIn () {
+
+
+	}
+
+	function signOut () {
+
+
+	}
+
+	function resetPassword () {
+
+
+	}
+
+
+}
+
+?>
