3b5d7c67ff9caf9200664b8974a53d6951854d12
Graeme Boy <graemeboy@gmail.com>
2014-06-29 13:39:02 +0200

    Update content and post models

--- app/models/Content.php
+++ app/models/Content.php
@@ -1,108 +1,293 @@
 <?php
 /**
  * ContentModel
- * 
+ *
+ * The two models that are indended to extend this class are Post and Page.
+ * There are minor differences between posts and pages in this CMS:
+ *     1. The data and content for each are stored in different locations.
+ *     2. They tend to appear in different views, pages in menus, and posts in archives, etc.
+ * This warrants the creation of separate models for the content types. However, they both
+ * have similar traits - save, delete, and a request for all tokens of its type.
+ * This abstract class defines the common functions, while extended classes are intended to set
+ * different directory paths for data, and provide a minimal number of idiosyncratic functions.
  */
-abstract class ContentModel
-{
+abstract class ContentModel {
+
     /* -----------------
      *  Fields
      * -----------------
      */
+
+    // The path for the summary data for each post or page.
     protected $summaryPath;
+    // The path in which HTML content is stored for quick access on the frontend.
     protected $htmlPath;
+    // The path in which markdown content is store for access when editing in the admin area.
     protected $markdownPath;
-    
+
     /* -----------------
      *  Constructors
      * -----------------
      */
-    function __construct () {
-        
-//        $this->setHtmlPath();
-//        $this->setMarkdownPath();
-//        $this->setSummaryPath();
-        
+
+    function __construct() {
+
     } // __construct()
-    
-    
+
     /* -----------------
-     *  Observer Methods
+     *  Observers
      * -----------------
      */
-    
+
     /**
-     * function getAllContent
-     * Return a PHP array of all the items of some content type.
-     *  If no data are available, returns an empty PHP array.
-     * @param string $summaryPath
-     * @return PHP array of the summaries of each item of content
+     * function getAll
+     * Return a PHP array of all the items of the current object's content type.
+     *
+     * @param string  $summaryPath, the directory for content summaries.
+     * @return PHP array of the summaries of each item of content. Empty array if none exist.
      */
-    function getAllContent ($summaryPath) {
-        
-        if (file_exists($summaryPath)) {
-            $json_data = file_get_contents($summaryPath);
-            // Return posts as a php array.
-            if (!empty($json_data)) {
-                return json_decode($json_data, true);    
+    function getAll() {
+        if ( file_exists( $this->summaryPath ) ) {
+            $json_data = file_get_contents( $this->summaryPath );
+            // Return data as a php array if there are any.
+            if ( !empty( $json_data ) ) {
+                return json_decode( $json_data, true );
             }
         }
-        // There are no posts yet, return an empty array.
-         return array();
-        
-    } // getAllContent ()
-    
+        // There are no posts or pages yet, return an empty array.
+        return array();
+    }
+
     /**
-     * function getContentMarkdown
-     * Given a slug for a post or page, return the markdown content
-     * @param string slug, the slug identifying the file needed
-     * @pre the file exists
-     * @return the markdown content for the post or page, if the file exists
+     * function getSummary
+     * Returns a PHP array of the summary of a single item of content, e.g. post.
+     *
+     * @param string  $slug, the unique identifier for content.
+     * @return the summary of content from the given slug.
      */
-    function getContentMarkdown($slug) {
-        $markdownFile = $this->$markdownPath . "$slug.md";
-        if (file_exists($markdownFile)) {
-            return file_get_contents($markdownFile);
-        } // if   
-    } // getContentMarkdown (string)
-    
-    function getContentHtml($slug) {
+    public function getSummary( $slug ) {
+        $content = $this->getAll();
+        return $content[$slug];
+    }
+
+    /**
+     * function getMarkdown
+     * Given a slug for a post or page, return the markdown content.
+     *
+     * @param string  slug, the slug identifying the file needed.
+     * @return the markdown content for the post or page, if the file exists.
+     */
+    function getMarkdown( $slug ) {
+        $markdownFile = $this->markdownPath . "$slug.md";
+        if ( file_exists( $markdownFile ) ) {
+            return file_get_contents( $markdownFile );
+        }
+    }
+
+    /**
+     * function getHtml
+     * Returns the HTML content for an item of content.
+     *
+     * @param string  $slug, the unique identifier for the content.
+     * @return string, the HTML text for the content.
+     */
+    function getHtml( $slug ) {
         $htmlFile = $this->htmlPath . "$slug.html";
-        if (file_exists($htmlFile)) {
-            return file_get_contents($htmlFile);
-        } // if
+        if ( file_exists( $htmlFile ) ) {
+            return file_get_contents( $htmlFile );
+        }
     }
-    
-    function getMarkdownPath() {
-        return $this->$markdownPath;
-    }
-    
-    function getHtmlPath() {
-        return $this->$HtmlPath;
-    }
-    
-    function getSummaryPath() {
-        return $this->$summaryPath;
-    }
-    
-    
-    /* ---------------
-     *  Mutator Methods
-     * ---------------
+
+    /**
+     * function getMarkDownPath
+     *
+     * @return string, the path to the markdown directory
      */
-    
-    function setHtmlPath ($htmlPath) {
+    function getMarkdownPath() {
+        return $this->markdownPath;
+    }
+
+    /**
+     * function getHtmlPath
+     *
+     * @return string, the path to the HTML directory
+     */
+    function getHtmlPath() {
+        return $this->HtmlPath;
+    }
+
+    /**
+     * function getSummaryPath
+     *
+     * @return string, the path to the summary directory
+     */
+    function getSummaryPath() {
+        return $this->summaryPath;
+    }
+
+
+    /* ------------
+     *  Mutators
+     * ------------
+     */
+
+    /**
+     * function setHTMLPath
+     *
+     * @param string $htmlPath, the path to the HTML content
+     */
+    function setHtmlPath( $htmlPath ) {
         $this->htmlPath = $htmlPath;
     }
-    
-    function setMarkdownPath($markdownPath) {
+
+    /**
+     * function setMarkdownPath
+     *
+     * @param string $markdownPath, the path to the markdown content
+     */
+    function setMarkdownPath( $markdownPath ) {
         $this->markdownPath = $markdownPath;
     }
-    
-    function setSummaryPath($summaryPath) {
+
+    /**
+     * function setSummaryPath
+     *
+     * @param string $summaryPath, the path to the content summaries
+     */
+    function setSummaryPath( $summaryPath ) {
         $this->summaryPath = $summaryPath;
     }
-    
-    
+
+    /**
+     * function appendSummary
+     *
+     * @param array $postData, contains the summary data for the new content.
+     * @post the summary data for the content is appended to the data structure for posts.
+     * @return void
+     */
+    function appendSummary( $postData ) {
+        // Separate string of tags into array, using ',' as delineator.
+        $tags = explode( ',', $postData['tags'] );
+        // Remove any whitespace around each tag.
+        $postData['tags'] = trimTags($tags);
+        // Get all of the posts as assoc array
+        $posts = $this->getAll();
+        // Set the array with item slug to the given post data
+        $posts[$postData['slug']] = $postData;
+        // Now save these data to the summary file
+        $this->saveDataToFile( $posts, $this->summaryPath );
+    }
+
+
+    /**
+     * function save
+     * Saves the post content to the appropriate structures and directories.
+     * @param array $postData, all data for the post.
+     */
+    function save( $contentData ) {
+        $slug = $contentData['slug'];
+        // Extract markdown content.
+        $markdown = $contentData['content'];
+        // Convert markdown content to HTML
+        $htmlContent = convert_md_to_html( $markdown );
+        // Remove content before saving summary.
+        unset( $contentData['content'] );
+        // Remove any existing post with the original slug
+        if ( !empty( $contentData['slug-original'] ) ) {
+            // The old slug must be deleted, if it's not the same as the new one.
+            if ( $contentData['slug-original'] !== $contentData['slug'] ) {
+                $this->delete( $contentData['slug-original'] );
+            } // if
+            // No need to save the original slug in the content summary.
+            unset( $contentData['slug-original'] );
+        } // if
+        // Save summary data.
+        $this->appendSummary( $contentData );
+        // Save HTML
+        $this->saveHTMLToFile( $htmlContent, $slug );
+        // Save Markdown
+        $this->saveMarkdownToFile( $markdown, $slug );
+    }
+
+    /**
+     * Takes a PHP array and saves it as an appropriate data structure
+     * to the file.
+     *
+     * @param unknown $data, a php array
+     */
+    function saveDataToFile( $data, $filename ) {
+        // Save contents of php arrry to the summary page as json.
+        file_put_contents( $filename, json_encode( $data ) );
+    } // save_summary (array)
+
+    /**
+     * function saveHTMLToFile
+     *
+     * @param unknown $htmlContent, string of html
+     * @param unknown $slug,        string
+     * @post an html file is created containing the given html
+     */
+    function saveHTMLToFile( $htmlContent, $slug ) {
+        file_put_contents( $this->htmlPath . "$slug.html", $htmlContent );
+    } // saveHTMLToFile (string, string)
+
+    /**
+     * function saveMarkdownToFile
+     *
+     * @param unknown $markdown, string of markdown text
+     * @param unknown $slug,     string
+     * @post a markdown file is created containing the given html
+     */
+    function saveMarkdownToFile( $markdown, $slug ) {
+        file_put_contents( $this->markdownPath . "$slug.md", $markdown );
+    } // saveMarkdownToFile (string, string)
+
+
+    /**
+     * function delete
+     * Removes all traces of the content.
+     *
+     * @param string  $slug the unique identifier of the content.
+     * @return void
+     */
+    function delete( $slug ) {
+        $content = $this->getAll();
+        if ( !empty( $content[$slug] ) ) {
+            unset( $content[$slug] );
+        } // if
+        $this->saveDataToFile( $content, $this->summaryPath );
+        // Remove the html file
+        $this->deleteHtml( $slug );
+        $this->deleteMarkdown( $slug );
+    }
+
+    /**
+     * function deleteHtml
+     * Unlinks the html page for a given content.
+     *
+     * @param string  $slug the unique identifier of the content.
+     */
+    function deleteHtml( $slug ) {
+        // HTML data is stored in an html file.
+        $filename = $this->$htmlPath . "$slug.html";
+        if ( file_exists( $filename ) ) {
+            unlink( $filename );
+        } // if
+    }
+
+    /**
+     * function deletMarkdown
+     * Unlinks the markdown file for a given content.
+     *
+     * @param string  $slug the unique identifier of the content.
+     */
+    function deleteMarkdown( $slug ) {
+        // HTML data is stored in an html file.
+        $filename = $this->markdownPath . "$slug.md";
+        if ( file_exists( $filename ) ) {
+            unlink( $filename );
+        } // if
+    }
+
 } // class ContentModel
 ?>
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once('ContentModel.php');
+require_once 'Content.php';
 
-class PostModel extends ContentModel
+class Post extends ContentModel
 {
     /*-------------
-     * Fields 
+     * Fields
      *------------
      */
     // Summary path, a path to some standard data file type (json, xml, yaml.)
     const SUMMARY_PATH = "posts/posts_summary.json";
     const HTML_PATH = "posts/html/";
     const MARKDOWN_PATH = "posts/markdown/";
-    
+
     /*-------------
      * Constructors
      *-------------
@@ -20,245 +20,63 @@ class PostModel extends ContentModel
     function __construct() {
         // Call the parent constructor
         parent::__construct();
-        
-        $this->setMarkdownPath(DATAPATH . self::MARKDOWN_PATH);
-        $this->setHtmlPath(DATAPATH . self::HTML_PATH);
-        $this->setSummaryPath(DATAPATH . self::SUMMARY_PATH);
-        
+
+        $this->setMarkdownPath( DATAPATH . self::MARKDOWN_PATH );
+        $this->setHtmlPath( DATAPATH . self::HTML_PATH );
+        $this->setSummaryPath( DATAPATH . self::SUMMARY_PATH );
+
+
     } // __construct ()
-    
+
     /* -----------------
-     *  Observers 
+     *  Observers
      * ----------------
      */
-    
-    /**
-     * Get all post summaries as a PHP array
-     * @return array of posts
-     */
-    public function getAllPosts() {
-        return $this->getAllContent($this->summaryPath);
-    } // get posts()
-    
-    /**
-     * function getPostSummary
-     * @param string slug
-     * @return the summary of a post from the given slug
-     */
-    public function getPostSummary($slug) {
-        $posts = $this->getAllPosts();
-        return $posts[$slug];
-    } // getPostSummary(string)
-    
+
     /**
      * function isPost
      * Returns true if a post is contained in the array of posts stored in summary.
-     * @param string $slug
+     *
+     * @param string  $slug
      * @return boolean isPost
      */
-    public function isPost($slug) {
+    public function isPost( $slug ) {
         // Get all post summaries (the smallest sets of data about posts)
         $posts = $this->getAllPosts();
         // Return boolean if a post exists with this slug.
-        return (!empty($posts[$slug]));        
+        return !empty( $posts[$slug] );
     } // isPost(string)
-    
-    /**
-     * function getPostMarkdown
-     * Gets the mardown data for a post, given a slug
-     * @pre HTML post content exists with slug in appropriate directory
-     * @return valid markdown for the post, based on html.
-     */
-    public function getPostMarkdown ($slug) {
-        //return $this->getContentMarkdown($slug);
-    } // get_post (string)
-    
-    /**
-     * Returns the HTML content for a given post slug
-     * @param slug, string
-     * @return html string for post with given slug
-     */
-    public function getPostHtml ($slug) {
-        return $this->getContentHtml($slug);
-    } // getPostHtml (string)
-    
+
+
     /* ------------------
      *  Mutator Methods
      * -----------------
      */
-    
-    /**
-     * Will append all post summaries given a new post summary.
-     * @pre no existing post should have the same slug. 
-     *  Otherwise it is overwritten.
-     */
-    function appendSummary ($postData) {
-        // Save tags as an array
-        $tags = explode(',', $postData['tags']);
-        if (!empty($tags)) {
-            foreach ($tags as $index=>$tag) {
-                $tags[$index] = trim($tag);
-            } // foreach
-            $postData['tags'] = $tags;
-        } // if
-        // Get all of the posts as assoc array
-        $posts = $this->getAllPosts();
-        // Set the array with item slug to the given post data
-        $posts[$postData['slug']] = $postData;
-        // Now save these data to the summary file
-        $this->saveDataToFile($posts, $this->$summaryPage);
-    } // appendSummary (array)
-    
-    
-    /**
-     * function deletePost
-     * Takes a post slug and removes it from summary, 
-     *  deletes the markdown and html files.
-     */
-    function deletePost($slug) {
-        /*
-         *  Delete post from summary
-         */
-        $posts = $this->getAllPosts();
-        if (!empty($posts[$slug])) {
-            unset($posts[$slug]);
-        } // if
-        $this->saveDataToFile($posts, $this->$summaryPage);
-        // Remove the html file
-        $this->deletePostHTML($slug);
-        $this->deletePostMarkdown($slug);
-    } // deletePost (string)
-    
-    /**
-     * Unlinks the html page for a given post.
-     */
-    function deletePostHTML ($slug) {
-        // HTML data is stored in an html file.
-        $filename = $this->$htmlPath . "$slug.html";
-        if (file_exists($filename)) {
-            unlink($filename);
-        } // if
-    } // deletePostHTML
 
-    /**
-     * Unlinks the html page for a given post.
-     */
-    function deletePostMarkdown ($slug) {
-        // HTML data is stored in an html file.
-        $filename = $this->$markdownPath . "$slug.md";
-        if (file_exists($filename)) {
-            unlink($filename);
-        } // if
-    } // deletePostMarkdown
-    
-    /**
-     * function savePostFile
-     * Puts post data as an array in to a standard data structure file 
-     * that has the same of the slug.
-     * @param $postData, php array
-     */
-    function savePostFile ($postData) {
-        $slug = $post_data['slug'];
-        $post_string = var_export($post_data, true);
-        $data = '<?php $post_data = ' . $post_string . '; ?>';
-        file_put_contents ($this->$htmlPath . $slug.php, $data);
-    } // save_html (array)
-    
-    
-    
-    /**
-     * Add a post to the posts "database"
-     */
-    public function savePost ($postData){
-        $slug = $postData['slug'];
-        
-        /*
-         * Set data for summary and HTML
-         */ 
-        $markdown = $postData['content'];
-        $htmlContent = convert_md_to_html($markdown);
-        unset($postData['content']); // remove any content
-        
-        // Remove any existing post with the original slug
-        if (!empty($postData['slug-original'])) {
-            // The old slug must be deleted.
-            if ($postData['slug-original'] !== $postData['slug']) {
-                $this->deletePost($postData['slug-original']);
-            } // if
-            unset($postData['slug-original']);
-        } // if
-        // Save summary data.
-        $this->appendSummary($postData);
-        /*
-         * Save HTML and Markdown content.
-         */
-        $this->saveHTMLToFile($htmlContent, $slug);
-        $this->saveMarkdownToFile($markdown, $slug);
-    } // savePost(array)
-   
-    
-    /**
-     * function removeOriginalPost
-     * Posts can be edited and slugs changed. The edit post page
-     * will send a "slug-original" value of the post's slug before
-     * the change, which will be deleted.
-     * @para $postData, includes param ['slug-original']
-     */
 
-    
-    /**
-     * Takes a PHP array and saves it as an appropriate data structure 
-     * to the file.
-     * @param $data, a php array
-     */
-    function saveDataToFile ($data, $filename) {
-        // Save contents of php arrry to the summary page as json.
-        file_put_contents ($filename, json_encode($data));
-    } // save_summary (array)
-    
-    /**
-     * function saveHTMLToFile
-     * @param $htmlContent, string of html
-     * @param $slug, string
-     * @post an html file is created containing the given html
-     */
-    function saveHTMLToFile($htmlContent, $slug) {
-        file_put_contents ($this->$htmlPath . "$slug.html", $htmlContent);
-    } // saveHTMLToFile (string, string)
-    
-    /**
-     * function saveMarkdownToFile
-     * @param $markdown, string of markdown text
-     * @param $slug, string
-     * @post a markdown file is created containing the given html
-     */
-    function saveMarkdownToFile($markdown, $slug) {
-        file_put_contents ($this->$markdownPath . "$slug.md", $markdown);
-    } // saveMarkdownToFile (string, string)
-    
     /**
      * function forceSafeSlug
      * Ensures that the slug can be used as it is.
      * This ought to have been done on the frontend, but is further
      * reinforced at this point.
      *
-     * @param string $str
-     * @param int $length, the maximum length of the slug
+     * @param string  $str
+     * @param int     $length, the maximum length of the slug
      * @return string
      */
-//    function forceSafeSlug($str, $length = 64) {
-//        $str = safeString(strip_tags($str));
-//
-//        $str = str_replace(" ", "-", $str);
-//        $str = strtolower(preg_replace("/[^a-zA-Z0-9_-]/i", "", $str));
-//        $str = preg_replace("/[-]+/i", "-", $str);
-//        if ($length > 0) {
-//            $str = substr($str, 0, $length);
-//        }
-//        $str = trim($str, " -"); // Make sure it doesn't start or end with '-'..
-//
-//        return $str;
-//    }
+    //    function forceSafeSlug($str, $length = 64) {
+    //        $str = safeString(strip_tags($str));
+    //
+    //        $str = str_replace(" ", "-", $str);
+    //        $str = strtolower(preg_replace("/[^a-zA-Z0-9_-]/i", "", $str));
+    //        $str = preg_replace("/[-]+/i", "-", $str);
+    //        if ($length > 0) {
+    //            $str = substr($str, 0, $length);
+    //        }
+    //        $str = trim($str, " -"); // Make sure it doesn't start or end with '-'..
+    //
+    //        return $str;
+    //    }
 } // class PostModel
 
 ?>
