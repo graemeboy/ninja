5bc3f38ad6c343bda6131dcd978a67519763c255
Graeme Boy <graemeboy@gmail.com>
2014-07-02 21:56:07 +0200

    Update routes

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -28,10 +28,13 @@ $router->get( '/admin/edit-posts/:slug', function ( $slug ) use ( $admin, $route
 $router->get( '/admin/edit-posts', function () use ( $admin, $router ) {
 		$admin->managePosts( $router );
 	} );
+$router->get( '/admin/edit-pages/:slug', function ( $slug ) use ( $admin, $router ) {
+		$admin->editPage( $router, $slug );
+	} );
 $router->get( '/admin/edit-pages', function () use ( $admin, $router ) {
 		$admin->managePages( $router );
 	} );
-$router->get( '/admin/edit-media', function () use ( $admin, $router ) {
+$router->get( '/admin/manage-media', function () use ( $admin, $router ) {
 		$admin->manageMedia( $router );
 	} );
 
@@ -40,14 +43,28 @@ $router->get( '/admin/settings-site', function () use ( $admin, $router ) {
 		$admin->settingsSite( $router );
 	} );
 // Appearance Settings - Theme and Styles
-$router->get( '/admin/appearance', function () use ( $admin, $router ) {
-		$admin->appearance( $router );
+$router->get( '/admin/layout-style', function () use ( $admin, $router ) {
+		$admin->layoutStyle( $router );
 	} );
+// Appearance Settings - Menu
+$router->get( '/admin/menu', function () use ( $admin, $router ) {
+		$admin->menu( $router );
+	} );
+// Appearance Settings - Widgets
+$router->get( '/admin/widgets', function () use ( $admin, $router ) {
+		$admin->widgets( $router );
+	} );
+// Delete post
 $router->get( '/admin/delete-post/:slug', function ( $slug ) use ( $admin, $router ) {
 		$admin->deletePost( $slug );
 		$router->redirect( '/admin/edit-posts' );
 	} );
-
+// Delete media
+$router->get( '/admin/delete-media/:slug', function ( $slug ) use ( $admin, $router ) {
+		$admin->deleteMedia( $slug );
+		$router->redirect( '/admin/manage-media' );
+	} );
+// View post - redirect
 $router->get( '/admin/view-post/:slug', function ( $slug ) use ( $router ) {
 		$router->redirect( "/$slug" );
 	} );
